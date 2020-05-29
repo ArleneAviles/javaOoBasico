0ffd5cda1b5bc2ae4487eccf2749fb4de8569bc0
Arlene Aviles <arlene.aviles@astrazeneca.com>
2020-05-29 15:32:14 -0500

    Clase Escuela

---
+++ src/com/testAutomationCoach/aAbstraccion/ArticuloAmazon.java
@@ -0,0 +1,27 @@
+package com.testAutomationCoach.aAbstraccion;
+
+public class Articulo {
+    String precio;
+    String departamento;
+    int tamanio;
+    double peso;
+    boolean existencia;
+    int cantidad;
+    boolean envio;
+    String nombre;
+    String identificador;
+
+
+
+    public void agregarCarrito(String myColor) {}
+
+    public void buscar(String myLabel) {}
+
+    public String comprar(String identicador)
+
+    public String compararlo(String identificador){}
+
+    public void eliminar(String identificador) {}
+
+
+}
